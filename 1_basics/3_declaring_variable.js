755a7964fb1ea116cbf6bc6578121c1d70f827c7
WONSY <sywon@inventis.co.kr>
2024-09-06 19:02:08 +0900

    javascript study

---
+++ 1_basics/3_declaring_variable.js
@@ -0,0 +1,43 @@
+/**
+ * Variable 선언하기
+ * 
+ * 1) var - 더이상 쓰지 않는다.
+ * 2) let
+ * 3) cosnt
+ */
+var name = 'Variavle 선언';
+console.log(name);
+
+var age = 32;
+console.log(age);
+
+let ive = '아이브';
+console.log(ive);
+
+/**
+ * let과 var로 선언하면
+ * 값을 추후 변경할 수 있다.
+ */
+
+ive = '안유진';
+console.log(ive);
+
+const newJeans = '뉴진스';
+console.log(newJeans);
+
+// newJeans='원서연';
+
+/**
+ * 선언과 할당
+ * 
+ * 1) 변수를 선언하는 것.
+ * var name;
+ * 2) 할당
+ * var name = '아이브';
+ */
+var name = '아이브';
+console.log(name);
+
+let girlFriend;
+console.log(girlFriend);
+
---
+++ 1_basics/5_data_types.js
@@ -0,0 +1,47 @@
+/**
+ * Data Types
+ * 
+ * 여섯개의 Primitive Type과
+ * 한개의 오브젝트 타입이 있다.
+ * 
+ * 1) Number (숫자)
+ * 2) String (문자열)
+ * 3) Boolean (불리언)
+ * 4) undefined (언디파인드)
+ * 5) null (널)
+ * 6) Symbol (심볼)
+ * 
+ * 7) Object (객체)
+ *    Function
+ *    Array
+ *    Object
+ */
+
+/**
+ * Number 타입
+ */
+const age = 32;
+const tempature = -10;
+const pi = 3.14;
+
+console.log(typeof age);
+console.log(typeof tempature);
+console.log(typeof pi);
+console.log('-----------------');
+
+const infinity = Infinity;
+const nInfinity = -Infinity;
+
+console.log(typeof infinity);
+console.log(typeof nInfinity);
+console.log('-----------------');
+
+/**
+ * String 타입
+ */
+ const codeFactory = '"코"드팩토리';
+ console.log(codeFactory);
+ console.log(typeof codeFactory);
+
+ const ive = "'아이브' 안유진";
+ console.log(ive);
